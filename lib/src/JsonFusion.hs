9bb795c136e98b9a251f2a058368ef4ef905ec59
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-01-23 09:30:05 +0000

    Clean: alignment

--- lib/src/JsonFusion.hs
+++ lib/src/JsonFusion.hs
@@ -17,8 +17,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE ConstraintKinds  #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module JsonFusion
   ( jsonFusion
